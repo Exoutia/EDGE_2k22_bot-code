b6b132ed37f12e7329b0fc9e6c64bfe17f4f0656
Exoutia <94464106+Exoutia@users.noreply.github.com>
2022-05-20 08:23:19 +0530

    Add files via upload

---
+++ harshita_zip/Bibek_forward.ino/Bibek_forward.ino.ino
@@ -0,0 +1,269 @@
+int s1;
+int s2;
+int s3;
+int s4;
+int s5;
+int s6;
+int s7;
+int s8;
+//int s9;
+
+const int trigPin = 8;
+const int echoPin = 7;
+const int LED1 = 9;
+
+int duration = 0;
+int distance = 0;
+
+
+const int ENA = 6;
+const int IN1=11;//right side of motor when you see bot from back
+const int IN2=10;
+const int ENB = 5;//left side of motor when you see bot from back
+const int IN3=13;
+const int IN4=12;
+void setup() 
+{
+  // put your setup code here, to run once:
+  pinMode(trigPin , OUTPUT);
+  pinMode(echoPin , INPUT);
+  pinMode(LED1 , OUTPUT);
+  pinMode(A0,INPUT);
+  pinMode(A1,INPUT);
+  pinMode(A2,INPUT);
+  pinMode(A3,INPUT);
+  pinMode(A4,INPUT);
+  pinMode(A5,INPUT);
+  pinMode(2,INPUT);
+  pinMode(3,INPUT);
+//  pinMode(4,INPUT);
+  
+
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
+  Serial.begin(9600);
+
+  //forward();
+  //delay(2000);
+}
+
+void loop() 
+{
+
+  
+  s1=digitalRead(A0);
+  s2=digitalRead(A1);
+  s3=digitalRead(A2);
+  s4=digitalRead(A3);
+  s5=digitalRead(A4);
+  s6=digitalRead(A5);
+  s7=digitalRead(2);
+  s8=digitalRead(3);
+//   s9=digitalRead(4);
+
+
+  Serial.print(s1);
+  
+  Serial.print(" ");
+  Serial.print(s2);
+  Serial.print(" ");
+  Serial.print(s3);
+  Serial.print(" ");
+  Serial.print(s4);
+  Serial.print(" ");
+  Serial.print(s5);
+  Serial.print(" ");
+  Serial.print(s6);
+  Serial.print(" ");
+  Serial.print(s7);
+  Serial.print(" ");
+  Serial.print(s8);
+  Serial.println(" ");
+  
+//  Serial.print(s9);
+//  Serial.println(" ");
+delay (1000);
+//  
+  
+  
+  
+  if(
+  ((s1==0)&&(s2==0)&&(s3==0)&&(s4==1)&&(s5==1)&&(s6==0)&&(s7==0)&&(s8==0))||
+  ((s1==0)&&(s2==0)&&(s3==1)&&(s4==1)&&(s5==1)&&(s6==1)&&(s7==0)&&(s8==0))||
+  ((s1==0)&&(s2==1)&&(s3==1)&&(s4==1)&&(s5==1)&&(s6==1)&&(s7==1)&&(s8==0))
+   //((s1==1)&&(s2==1)&&(s3==1)&&(s4==1)&&(s5==1)&&(s6==1)&&(s7==1)&&(s8==1))
+  )
+  {
+    forward();
+    
+  }
+  //right
+  else if(
+  ((s1==0)&&(s2==1)&&(s3==1)&&(s4==1)&&(s5==1)&&(s6==1)&&(s7==1)&&(s8==1))||
+  ((s1==0)&&(s2==0)&&(s3==1)&&(s4==1)&&(s5==1)&&(s6==1)&&(s7==1)&&(s8==1))||
+  ((s1==0)&&(s2==0)&&(s3==0)&&(s4==1)&&(s5==1)&&(s6==1)&&(s7==1)&&(s8==1))||
+  ((s1==0)&&(s2==0)&&(s3==0)&&(s4==0)&&(s5==1)&&(s6==1)&&(s7==1)&&(s8==1))||
+  ((s1==0)&&(s2==0)&&(s3==0)&&(s4==0)&&(s5==0)&&(s6==1)&&(s7==1)&&(s8==1))||
+  ((s1==0)&&(s2==0)&&(s3==0)&&(s4==0)&&(s5==0)&&(s6==0)&&(s7==1)&&(s8==1))||
+  ((s1==0)&&(s2==0)&&(s3==0)&&(s4==0)&&(s5==0)&&(s6==0)&&(s7==0)&&(s8==1))||
+  ((s1==0)&&(s2==0)&&(s3==0)&&(s4==0)&&(s5==0)&&(s6==1)&&(s7==1)&&(s8==0))||
+  ((s1==0)&&(s2==0)&&(s3==0)&&(s4==0)&&(s5==1)&&(s6==1)&&(s7==0)&&(s8==0))||
+  ((s1==0)&&(s2==0)&&(s3==0)&&(s4==0)&&(s5==1)&&(s6==1)&&(s7==1)&&(s8==0))||
+  ((s1==0)&&(s2==0)&&(s3==0)&&(s4==1)&&(s5==1)&&(s6==1)&&(s7==0)&&(s8==0))||
+  ((s1==0)&&(s2==0)&&(s3==0)&&(s4==1)&&(s5==1)&&(s6==1)&&(s7==1)&&(s8==0))||
+  ((s1==0)&&(s2==0)&&(s3==1)&&(s4==1)&&(s5==1)&&(s6==1)&&(s7==1)&&(s8==0))||
+  ((s1==1)&&(s2==0)&&(s3==1)&&(s4==1)&&(s5==1)&&(s6==1)&&(s7==1)&&(s8==1))||
+  ((s1==1)&&(s2==1)&&(s3==0)&&(s4==1)&&(s5==1)&&(s6==1)&&(s7==1)&&(s8==1))||
+  ((s1==1)&&(s2==1)&&(s3==1)&&(s4==0)&&(s5==1)&&(s6==1)&&(s7==1)&&(s8==1))||
+  ((s1==1)&&(s2==0)&&(s3==0)&&(s4==1)&&(s5==1)&&(s6==1)&&(s7==1)&&(s8==1))||
+  ((s1==1)&&(s2==1)&&(s3==0)&&(s4==0)&&(s5==1)&&(s6==1)&&(s7==1)&&(s8==1))||
+  ((s1==1)&&(s2==0)&&(s3==0)&&(s4==0)&&(s5==1)&&(s6==1)&&(s7==1)&&(s8==1))||
+  ((s1==1)&&(s2==1)&&(s3==0)&&(s4==0)&&(s5==0)&&(s6==1)&&(s7==1)&&(s8==1))||
+  ((s1==1)&&(s2==0)&&(s3==0)&&(s4==0)&&(s5==0)&&(s6==1)&&(s7==1)&&(s8==1))||
+  ((s1==1)&&(s2==0)&&(s3==0)&&(s4==0)&&(s5==0)&&(s6==0)&&(s7==1)&&(s8==1))
+  )
+  {
+    right();
+  }
+  else if(
+  ((s1==1)&&(s2==1)&&(s3==1)&&(s4==1)&&(s5==1)&&(s6==1)&&(s7==1)&&(s8==0))||
+  ((s1==1)&&(s2==1)&&(s3==1)&&(s4==1)&&(s5==1)&&(s6==1)&&(s7==0)&&(s8==0))||
+  ((s1==1)&&(s2==1)&&(s3==1)&&(s4==1)&&(s5==1)&&(s6==0)&&(s7==0)&&(s8==0))||
+  ((s1==1)&&(s2==1)&&(s3==1)&&(s4==1)&&(s5==0)&&(s6==0)&&(s7==0)&&(s8==0))||
+  ((s1==1)&&(s2==1)&&(s3==1)&&(s4==0)&&(s5==0)&&(s6==0)&&(s7==0)&&(s8==0))||
+  ((s1==1)&&(s2==1)&&(s3==0)&&(s4==0)&&(s5==0)&&(s6==0)&&(s7==0)&&(s8==0))||
+  ((s1==1)&&(s2==0)&&(s3==0)&&(s4==0)&&(s5==0)&&(s6==0)&&(s7==0)&&(s8==0))||
+  ((s1==0)&&(s2==1)&&(s3==1)&&(s4==0)&&(s5==0)&&(s6==0)&&(s7==0)&&(s8==0))||
+  ((s1==0)&&(s2==0)&&(s3==1)&&(s4==1)&&(s5==0)&&(s6==0)&&(s7==0)&&(s8==0))||
+  ((s1==0)&&(s2==1)&&(s3==1)&&(s4==1)&&(s5==0)&&(s6==0)&&(s7==0)&&(s8==0))||
+  ((s1==0)&&(s2==0)&&(s3==1)&&(s4==1)&&(s5==1)&&(s6==0)&&(s7==0)&&(s8==0))||
+  ((s1==0)&&(s2==1)&&(s3==1)&&(s4==1)&&(s5==1)&&(s6==0)&&(s7==0)&&(s8==0))||
+  ((s1==0)&&(s2==1)&&(s3==1)&&(s4==1)&&(s5==1)&&(s6==1)&&(s7==0)&&(s8==0))||
+  ((s1==1)&&(s2==1)&&(s3==1)&&(s4==1)&&(s5==0)&&(s6==1)&&(s7==1)&&(s8==1))||
+  ((s1==1)&&(s2==1)&&(s3==1)&&(s4==1)&&(s5==1)&&(s6==0)&&(s7==1)&&(s8==1))||
+  ((s1==1)&&(s2==1)&&(s3==1)&&(s4==1)&&(s5==1)&&(s6==1)&&(s7==0)&&(s8==1))||
+  ((s1==1)&&(s2==1)&&(s3==1)&&(s4==1)&&(s5==0)&&(s6==0)&&(s7==1)&&(s8==1))||
+  ((s1==1)&&(s2==1)&&(s3==1)&&(s4==1)&&(s5==1)&&(s6==0)&&(s7==0)&&(s8==1))||
+  ((s1==1)&&(s2==1)&&(s3==1)&&(s4==0)&&(s5==0)&&(s6==0)&&(s7==1)&&(s8==1))||
+  ((s1==1)&&(s2==1)&&(s3==1)&&(s4==1)&&(s5==0)&&(s6==0)&&(s7==0)&&(s8==1))||
+  ((s1==1)&&(s2==1)&&(s3==1)&&(s4==0)&&(s5==0)&&(s6==0)&&(s7==0)&&(s8==1))||
+  ((s1==1)&&(s2==1)&&(s3==0)&&(s4==0)&&(s5==0)&&(s6==0)&&(s7==0)&&(s8==1))
+  
+  )
+  {
+    left();
+  }
+  else if((s1==0)&&(s2==0)&&(s3==0)&&(s4==0)&&(s5==0)&&(s6==0)&&(s7==0)&&(s8==0)){ 
+    //motor_stop();
+   //delayMicroseconds(1);
+    motor_rev(); 
+    delayMicroseconds(2);
+  }
+  else if((s1==1)&&(s2==1)&&(s3==1)&&(s4==1)&&(s5==1)&&(s6==1)&&(s7==1)&&(s8==1)){
+   forward();
+  delayMicroseconds(150);
+    analogWrite(LED1, 255);
+     delayMicroseconds(1);
+   analogWrite(LED1, 0);
+   
+    
+  if((s1==1)&&(s2==1)&&(s3==1)&&(s4==1)&&(s5==1)&&(s6==1)&&(s7==1)&&(s8==1)){
+    motor_stop();
+  }
+  
+  }
+    
+     
+    
+  else {
+    forward1();
+   
+    }
+  
+//  digitalWrite(trigPin, LOW);     //here things are commented out with help of slash
+//  delayMicroseconds(2);
+//  digitalWrite(trigPin, HIGH);
+//  delayMicroseconds(10);
+//  digitalWrite(trigPin, LOW);  
+//  duration = pulseIn(echoPin, HIGH);
+//  distance = duration * 0.034/2;
+//  Serial.print("distance:----");
+//  
+//Serial.println(distance);
+//  
+//  if ( distance <= 10 )
+//  { 
+//    digitalWrite(LED1, HIGH);
+//    delayMicroseconds(1);
+//    
+//  }
+//  else
+//  {
+//    digitalWrite(LED1, LOW);
+//  }
+  
+  
+}
+void forward()
+{
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 100);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, 100);
+   //delayMicroseconds(1);
+}
+void forward1()
+{
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 90);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, 90);
+   //delayMicroseconds(1);
+}
+
+void right()
+{
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 110);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENB, 80);
+  //delayMicroseconds(5);
+}
+
+void left()
+{
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  analogWrite(ENA, 80);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, 110);
+  //delayMicroseconds(5);
+}
+
+void motor_stop()
+{
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 0);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, 0);
+}
+void motor_rev()
+{
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  analogWrite(ENA, 100);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENB, 100);
+}
---
+++ harshita_zip/pid__ram/pid__ram.ino
@@ -0,0 +1,261 @@
+int l1=9;
+int l2=8;
+
+int r1=11;
+int r2=10;
+
+int enl=5;
+int enr=6;
+
+int a[8];
+int last_proportional=0;
+int integral=0;
+
+
+//bool leds=false;
+
+
+char select_turn(unsigned char found_left,unsigned char found_right,unsigned char found_st); //AI INITIALIZAION
+
+int mod(int v); // MODULUS FUNCTION
+int set_motors(int a,int b);
+void turn(char dir);
+void PID();
+
+ int right=0;
+ int left=0;
+
+void setup() {
+
+  pinMode(l1,OUTPUT);   // Left motor output
+  pinMode(l2,OUTPUT);
+  
+  pinMode(r1,OUTPUT);  //Right motor output
+  pinMode(r2,OUTPUT);
+
+  pinMode(enl,OUTPUT);   //Enable pins of R and L motors
+  pinMode(enr,OUTPUT);
+  
+  pinMode(12,INPUT);
+  pinMode(13,INPUT);
+  
+  pinMode(A0,INPUT);
+  pinMode(A1,INPUT);
+  pinMode(A2,INPUT);
+  pinMode(A3,INPUT);
+  pinMode(A4,INPUT);
+  pinMode(A5,INPUT);
+  pinMode(2,INPUT);
+  pinMode(3,INPUT);
+  Serial.begin(9600);
+
+ // pinMode(ledg,OUTPUT);
+  //pinMode(ledr,OUTPUT);
+//  digitalWrite(ledg,leds);
+ // digitalWrite(ledr,leds);
+}
+
+void loop() {// stop();
+  PID();    //Control invoked
+ 
+  unsigned char found_left=0;
+  unsigned char found_right=0;   // turn paratmeter.
+  unsigned char found_st=0;
+
+readline();
+  if(a[0]==LOW)
+    found_left=1;
+    
+  if(a[7]==LOW)
+    found_right=1;
+ 
+  if(a[3]==LOW||a[4]==LOW)
+  found_st=1;
+
+  unsigned char dir;
+
+  dir=select_turn(found_left, found_right, found_st);
+  turn(dir);
+
+
+
+}// MAIN body ends
+
+
+
+int set_motors(int l,int r) // Motor setup
+{
+  Serial.println(r);
+  Serial.println(l);
+
+
+if(l>0&&r>0)
+{
+  analogWrite(enl,mod(l));
+analogWrite(enr,mod(r));
+
+    
+  digitalWrite(l1,LOW);
+  digitalWrite(l2,HIGH);
+  
+  digitalWrite(r1,LOW);
+  digitalWrite(r2,HIGH);
+}
+
+else if(l<0&&r>0)
+{
+  analogWrite(enl,mod(l));
+analogWrite(enr,mod(r));
+
+  digitalWrite(l1,HIGH);
+  digitalWrite(l2,LOW);
+  
+  digitalWrite(r1,LOW);
+  digitalWrite(r2,HIGH);
+}
+
+else if(l>0&&r<0)
+{analogWrite(enl,mod(l));
+analogWrite(enr,mod(r));
+
+  digitalWrite(l1,LOW);
+  digitalWrite(l2,HIGH);
+  
+  digitalWrite(r1,HIGH);
+  digitalWrite(r2,LOW);
+}
+
+else if(l==0&&r==0)
+{
+  analogWrite(enl,0);
+analogWrite(enr,0);
+
+  digitalWrite(l1,HIGH);
+  digitalWrite(l2,HIGH);
+  
+  digitalWrite(r1,HIGH);
+  digitalWrite(r2,HIGH);
+}
+}
+
+int readline()
+{
+  a[0]=analogRead(A0);
+  a[1]=analogRead(A1);
+  a[2]=analogRead(A2);
+  a[3]=analogRead(A3);
+  a[4]=analogRead(A4);
+  a[5]=analogRead(A5);
+  a[6]=digitalRead(2);
+  a[7]=digitalRead(3);
+  int v;
+ v=(7000*a[0]+6000*a[1]+5000*a[2]+4000*a[3]+3000*a[4]+2000*a[5]+1000*a[6]+0*a[7])/(a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]);
+ Serial.println(a[7]);
+ return v;
+}
+
+int stop()
+{
+  analogWrite(enl,255);
+  analogWrite(enr,255);
+   digitalWrite(l1,HIGH);
+  digitalWrite(l2,HIGH);
+  
+  digitalWrite(r1,HIGH);
+  digitalWrite(r2,HIGH);
+  
+}
+
+void turn(char dir)   //Turning setup
+{
+  switch(dir)
+  {
+    case 'L':
+      set_motors(-200,200);
+      delay(350);
+      break;
+    case 'R':
+      set_motors(200,-200);
+      delay(350);
+      break;
+    case 'B':
+      set_motors(-200,-200);
+      delay(650);
+      break;
+    case 'S':
+      set_motors(200,200);
+      delay(650);
+      break;
+  }
+    
+ }
+
+
+void PID()
+{ int i;       // Control function
+  int power_difference=0;
+  float Kp,Ki,Kd;
+  unsigned int position;
+  int derivative,proportional;
+  while(1)
+  { 
+  position=readline();
+  Serial.println(position);
+  proportional=((int)position-3500);
+  
+   derivative = proportional-last_proportional;
+   integral = integral+proportional;
+
+  last_proportional=proportional;
+  Kp=0.1;
+  Ki=0.0001;
+  Kd=1.0;
+
+  power_difference= proportional*Kp + integral*Ki + derivative*Kd;
+  const int max=180;
+  if(power_difference>max)
+   power_difference=max;
+  if(power_difference<-max)
+   power_difference= (-1*max);
+   
+  if(power_difference<0)
+  {//left
+   set_motors(max+power_difference,max);
+  }
+  else
+  {
+   set_motors(max,max-power_difference); //right
+  }
+  readline();
+  if(a[0]==HIGH&&a[1]==HIGH&&a[2]==HIGH&&a[3]==HIGH&&a[4]==HIGH&&a[5]==HIGH&&a[6]==HIGH&&a[7]==HIGH)
+    return;
+  
+  else if(a[0]==LOW||a[7]==LOW)
+    return;
+  
+
+ 
+  }  
+}
+
+char select_turn(unsigned char found_left,unsigned char found_right,unsigned char found_st) // AI Function
+{
+ if(found_left==1)
+ return 'L';
+ else if(found_st==1)
+ return 'S';
+ else if(found_right==1)
+ return 'R';
+ else
+ return 'B';
+ 
+}
+
+
+int mod(int v)
+{
+  if(v<0)
+  return -1*v;
+  else if(v>0)
+  return v;
+}
